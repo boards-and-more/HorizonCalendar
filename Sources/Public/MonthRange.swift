70d7dfd3e5140e3adcad7f2c6e90a9bb9c31a78e
Philipp Rembold <philipp@macuja.com>
2022-11-17 15:36:40 +0100

    public keyword for some initializers

--- Sources/Public/MonthRange.swift
+++ Sources/Public/MonthRange.swift
@@ -25,7 +25,7 @@ extension MonthRange {
 
   /// Instantiates a `MonthRange` that encapsulates the `dateRange` in the `calendar` as closely as possible. For example,
   /// a date range of [2020-01-19, 2021-02-01] will result in a month range of [2020-01, 2021-02].
-  init(containing dateRange: ClosedRange<Date>, in calendar: Calendar) {
+  public init(containing dateRange: ClosedRange<Date>, in calendar: Calendar) {
     self.init(
       uncheckedBounds: (
         lower: calendar.month(containing: dateRange.lowerBound),
